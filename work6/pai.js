275a9c4f621c4742a6b19fc465502f448a1b4583
MonsterNB <2368681745@qq.com>
2019-10-23 18:03:09 +0800

    zzz

--- work6/pai.js
+++ work6/pai.js
@@ -14,24 +14,24 @@ function factorial(n){
     let sum = 1
     //for循环累乘
     for(let i=1;i<=n;++i){
-        sum *=1
+        sum *=i
     }
     //返回阶乘汇总数
     return sum
 }
-//声明奇数阶乘函数(2n=1)!! oddFactorial()
-function oddFactorial(n){
+//声明奇数阶乘函数(2n+1)!! oddFactorial()
+function oddFactrial(n){
     //声明阶乘汇总数并赋值为1
     let sum = 1
     //for循环累乘
-    for(let i=1;i<=n;++1){
+    for(let i=1;i<=n;++i){
         sum *= ((2*i)+1)
     }
     //返回阶乘汇总数
     return sum
 }
 //阶乘法计算圆周率函数
-function pi(sum){
+function pi(num){
     //声明汇总值变量，并且赋值0
     let sum = 0
     //for循环计算圆周率
@@ -39,7 +39,7 @@ function pi(sum){
         //声明阶乘被除数 dividend, 并调用factorial函数取值
         let dividend = factorial(i)
         //声明阶乘被除数 divisor, 并调用oddFactorial函数取值
-        let divisor = oddFactorial(i)
+        let divisor = oddFactrial(i)
         //累加汇总数
         sum += (dividend/divisor)
     }
